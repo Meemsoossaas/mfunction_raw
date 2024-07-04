a858fe4b5d0e3c1c21ebc779caff5e73fab53775
Meemsoossaas <rayyankom@gmail.com>
2024-07-04 13:48:49 +0200

    Commit (04/07/2024)

--- bin/test/function_test.dart
+++ bin/test/function_test.dart
@@ -2,6 +2,6 @@ import '../methods/methods.dart' show analyzeFunction;
 
 void main() async {
   String function =
-      "(8/2 * x - 2)";
+      "((x + 1)/(x - 2))";
   await analyzeFunction(function);
 }
